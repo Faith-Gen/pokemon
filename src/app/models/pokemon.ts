baa7951d9cef143d6cc8f302abb3274eb7006b9f
innoflash <innocentmazando@gmail.com>
2020-06-22 15:47:16 +0200

    added moves

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -2,6 +2,7 @@ import {Ability} from './pokemon/ability';
 import {Data} from './pokemon/data';
 import {Type} from './pokemon/type';
 import {Sprites} from './pokemon/sprites';
+import {Move} from './pokemon/move';
 
 export interface Pokemon extends Data {
   base_experience: number;
@@ -11,4 +12,5 @@ export interface Pokemon extends Data {
   weight: number;
   height: number;
   sprites: Sprites;
+  moves: Move[];
 }
